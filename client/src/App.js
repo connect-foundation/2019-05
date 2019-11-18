ea48e3538932962c8f84010470a6fdefc8072c12
eastgerm <dowk0331@gmail.com>
2019-11-18 13:39:19 +0900

    BUILD: CRA를 통한 client 폴더 재생성
    자체 webpack설정을 통해 React 환경을 조성하려 했으나
    CRA에서 제공하는 설정만으로도 충분하다 판단하여
    CRA를 통하여 React환경을 조성한 client 폴더를 새로 만들었음
    closed #18

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          QuickKick init
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
