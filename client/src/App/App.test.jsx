94934d0892f9148516ce932e577898d494c6a99f
Sam Rho <seungnam2@gmail.com>
2019-11-20 14:12:54 +0900

    TEST: test 환경 구축
    1. travis-CI를 돌릴 때 *.test.jsx에 실질적인 테스트가 하나도 없어 에러가 뜨던 부분 수정
    2. VSCode에서 IntelliSense의 도움을 받아 test code의 자동완성 기능을 사용하기 위해
       jsconfig.json을 만들었고, 그 file은 .gitignore에 넣음
    3. @testing-library를 다시 설치함
    4. footer, header, navbar에 각각 기본적인 test를 넣음.
    5. index.jsx -> index.js로 이름 변경

--- client/src/App/App.test.jsx
+++ client/src/App/App.test.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
+import '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
+
