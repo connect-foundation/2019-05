caabbd6353eb00d68b1819b851b6cb860c9dc519
Sam Rho <seungnam2@gmail.com>
2019-11-20 16:57:59 +0900

    FEATURE: Footer 컴포넌트 레이아웃 완성
    Footer 내category component 배치 완료. 추후에 footer에 들어갈 내용이
    결정되면 업데이트 필요

--- client/src/components/common/Footer/index.jsx
+++ client/src/components/common/Footer/index.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Footer.scss';
 
-const Footer = () => {
-  return (
-    <div className="footer">
-      <footer className="footer__message">
-        I'm your footer
-      </footer>
-    </div>
-  );
-};
+
+const Footer = () => (
+  <footer className="footer">
+    <FooterCategoryBlock title="Footer" contents={["this", "is", "footer"]} />
+  </footer>
+);
+
+
+const FooterCategoryBlock = ({ title, contents }) => (
+  <Fragment className="footer__list">
+    <h1 className="footer__list-header">{title}</h1>
+    <ul className="footer__list-items">
+      {contents.map(content => <li>{content}</li>)}
+    </ul>
+  </Fragment>
+)
+
 
 export default Footer;
