6a93fe4bafeb3ba7ff37e4872bbf49ef184ca932
eastgerm <dowk0331@gmail.com>
2019-11-20 14:05:08 +0900

    STRUCTURE: /components 하위에 위치한 폴더들에 index.js 생성
    /components 하위 폴더들을 모두 취합하여 하나의 객체로 내보내주는index.js 생성
    이를 통해 import시 해당 폴더의 필요한 component들을 한줄만으로 사용하여 불러올 수 있음

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Header from '../components/common/Header';
-import Footer from '../components/common/Footer';
+import {Header, Footer} from '../components/common';
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/common/index.js
@@ -0,0 +1,9 @@
+import Header from './Header';
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
+export {
+  Header,
+  Footer,
+  NavBar
+}
