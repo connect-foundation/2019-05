bbecbaffb1322320b00741aa1e7c55bc3c9d2ee9
Sam Rho <seungnam2@gmail.com>
2019-11-21 18:43:37 +0900

    TEST: setupTests.js file 추가
    추가해 준 뒤 내부에 testing-library와 enzyme import 해줌.

---
+++ client/src/setupTests.js
@@ -0,0 +1,10 @@
+import {
+  configure
+} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+configure({
+  adapter: new Adapter()
+});
