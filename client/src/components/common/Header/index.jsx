790a24e21ee97c3940fde94db392de6f9676db8c
eastgerm <dowk0331@gmail.com>
2019-11-21 18:39:42 +0900

    FEATURE: cookie를 통한 로그인 확인 후 NavBar에 알맞은 버튼 보여주기
    로그인 확인 전 -> 로그인 버튼
    로그인 확인 후 -> 로그아웃 버튼

--- client/src/components/common/Header/index.jsx
+++ client/src/components/common/Header/index.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Header.scss';
-import logo from "../../../assets/images/quickkick-logo.png";
+import logo from '../../../assets/images/quickkick-logo.png';
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        <ServiceLogo/>
+        <ServiceLogo />
       </div>
       <div className="header__right">
-        <NavBar/>
+        <NavBar />
       </div>
     </div>
   );
 };
 
 const ServiceLogo = () => {
-  return (
-    <img className="logo" src={logo} alt="퀵킥 로고"/>
-  );
+  return <img className="logo" src={logo} alt="퀵킥 로고" />;
 };
 
 const NavBar = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const cookie = document.cookie.split('=')[1];
+    setIsLogin(cookie === 'true');
+  }, []);
+
   return (
     <nav className="nav-bar">
       <div className="nav-bar__button">Quick Match</div>
       <div className="nav-bar__button">Quick Team</div>
-      <UserIcon className="nav-bar__userInfo"/>
+      {isLogin ? <UserIcon /> : <LoginBtn />}
     </nav>
   );
 };
 
-const UserIcon = (className) => {
+const LoginBtn = () => {
   return (
-    <div className={className}>
-      유저아이콘
+    <div className="nav-bar__userInfo">
+      <a href="http://127.0.0.1:4000/auth/naver">로그인</a>
     </div>
   );
+};
+
+const UserIcon = () => {
+  return <div className="nav-bar__userInfo">로그아웃</div>;
 };
 
 export default Header;
