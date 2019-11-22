6e40d295514ebfee17b5e95ea0a5569af30aee64
jong yoon <dhorlawhddbs@naver.com>
2019-11-22 10:53:07 +0900

    Review: review를 받을 파일 선정
    리뷰를 받을 파일 목록
     - .travis.yml
     - diff_checking.sh

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,4 +0,0 @@
-## 체크포인트
- - [ ] first task
- - [ ] second task
- - [ ] third task
--- client/src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header';
-import Footer from "./Footer";
-
-export {
-  Header,
-  Footer
-}
