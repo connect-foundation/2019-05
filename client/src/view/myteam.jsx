564c59baaca18e26917bb65d860231449c879103
eastgerm <dowk0331@gmail.com>
2019-11-25 16:10:11 +0900

    FIX: /src/view/myteam.jsx 코드 prettier 적용
    전체적인 통일성을 위해 prettier를 적용했습니다

--- client/src/view/myteam.jsx
+++ client/src/view/myteam.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Header, Footer} from '../components/common';
-const Myteam = () => {
-  return (
-    <div className="myTeam">
-      <Header />
-        my team is suck;
-      <Footer />
-    </div>
-  )
-};
+import { Header, Footer } from '../components/common';
+
+const Myteam = () => (
+  <div className="myTeam">
+    <Header />
+    <div>내팀내팀내팀</div>
+    <Footer />
+  </div>
+);
 
 export default Myteam;
