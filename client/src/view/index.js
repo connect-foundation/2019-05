9b4400c6d194f09bcbc7bde4a0f0f824382aa327
Woongki Rafael Kim <doom0728@gmail.com>
2019-11-25 15:30:41 +0900

    CHORE: 헤더 스타일 수정, myteam router 추가
    1200px 컨테이너 클래스 추가(.grid-container)
    컨테이너 추가에 따른 헤더 스타일링 조정
    myteam router 추가

--- client/src/view/index.js
+++ client/src/view/index.js
@@ -1,9 +1,6 @@
 import home from './home';
 import match from './match';
 import team from './team';
+import myteam from './myteam';
 
-export {
-  home,
-  match,
-  team,
-};
+export { home, match, team, myteam };
---
+++ client/src/view/myteam.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Header, Footer} from '../components/common';
+const Myteam = () => {
+  return (
+    <div className="myTeam">
+      <Header />
+        my team is suck;
+      <Footer />
+    </div>
+  )
+};
+
+export default Myteam;
