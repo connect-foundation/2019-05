ebec6d07d0d2218a38d49791a2218466e7368913
Sam Rho <seungnam2@gmail.com>
2019-11-26 14:54:37 +0900

    FEATURE: title을 나타낼 component 추가
    props로 title을 전달하면 받아서 h2로 나타내는 코드 추가

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,4 +1,7 @@
 import Header from './Header';
 import Footer from './Footer';
+import ViewTitle from './ViewTitle';
+import MatchFilter from './MatchFilter';
+import Button from './Button';
 
-export { Header, Footer };
+export { Header, Footer, ViewTitle, MatchFilter, Button };
