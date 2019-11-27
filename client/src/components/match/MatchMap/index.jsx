90520a7b7dc9110e42976f7e90c82209aed7024c
jong yoon <dhorlawhddbs@naver.com>
2019-11-27 17:13:45 +0900

    FEATURE: MatchMap 컴포넌트에 naver 지도 보여주기
    MatchMap 컴포넌트에서 서울 지역 한정 맵을 보여주기 위해
    naver map api를 사용하여 지도 생성 완료.

--- client/src/components/match/MatchMap/index.jsx
+++ client/src/components/match/MatchMap/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import loadJs from 'load-js';
+
 import './index.scss';
+
+const loadNaverMap = () => {
+  const requestURL = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_NCP_CLIENT_ID}`;
+  return loadJs(requestURL).then(() => {
+    const naverMap = window.naver.maps;
+    if (naverMap.jsContentLoaded) {
+      return naverMap;
+    }
+    return new Promise((resolve) => {
+      naverMap.onJSContentLoaded = () => {
+        resolve(naverMap);
+      };
+    });
+  });
+};
+
+const showNaverMap = () => {
+  return (async () => {
+    const loadedMap = await loadNaverMap();
+    const naverMap = new loadedMap.Map('naver-map', {
+      useStyleMap: true,
+      zoom: 10,
+      center: new loadedMap.LatLng(37.5666103, 126.9783882),
+    });
+  })();
+};
+
 const MatchMap = () => {
-  return <div className="match-map">이거슨 맵입니다.</div>;
+  showNaverMap();
+  return (
+    <div className="match-map">
+      <div id="naver-map" />
+    </div>
+  );
 };
 
 export default MatchMap;
