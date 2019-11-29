c47b3f4cfeada3944fc8183985e88bc637ca23bb
Sam Rho <seungnam2@gmail.com>
2019-11-29 11:30:11 +0900

    REVIEW: 필요없는 디렉토리 삭제

--- client/src/components/home/index.js
+++
@@ -1,6 +0,0 @@
-import HomeQuickMatch from './HomeQuickMatch';
-import HomeMatchStatus from './HomeMatchStatus';
-import HomeTeamRanking from './HomeTeamRanking';
-import HomeIntroduce from './HomeIntroduce';
-
-export { HomeQuickMatch, HomeMatchStatus, HomeTeamRanking, HomeIntroduce };
--- client/src/components/match/MatchMap/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.scss';
-
-const MatchMap = () => {
-  return <div className="match-map">이거슨 맵입니다</div>;
-};
-
-export default MatchMap;
