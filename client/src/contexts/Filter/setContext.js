feb5cf5ed98a623311096e5ab623c0663d569a2e
eastgerm <dowk0331@gmail.com>
2019-12-03 21:53:45 +0900

    FEATURE: FilterContext에 정의된 state 수정
    날짜, 시간, 랭크 에 관한 state와 조작 함수 추가
    아직 지역에 관한 state는 없으나 곧 넣을 예정

--- client/src/contexts/Filter/setContext.js
+++ client/src/contexts/Filter/setContext.js
@@ -1,31 +1,19 @@
 import moment from 'moment';
 
 const setFilterContext = {
-  setCheckedSN: (setFilterState) => {
-    setFilterState((prev) => ({
-      ...prev,
-      isCheckedSN: !prev.isCheckedSN,
-    }));
-  },
-  setCheckedSB: (setFilterState) => {
-    setFilterState((prev) => ({
-      ...prev,
-      isCheckedSB: !prev.isCheckedSB,
-    }));
-  },
-  setCheckedDB: (setFilterState) => {
-    setFilterState((prev) => ({
-      ...prev,
-      isCheckedDB: !prev.isCheckedDB,
-    }));
-  },
-  setCheckedDN: (setFilterState) => {
-    setFilterState((prev) => ({
-      ...prev,
-      isCheckedDN: !prev.isCheckedDN,
-    }));
-  },
   setMatchDay: (setFilterState, payload) => {
+    setFilterState((prev) => ({
+      ...prev,
+      ...payload,
+    }));
+  },
+  setStartTime: (setFilterState, payload) => {
+    setFilterState((prev) => ({
+      ...prev,
+      ...payload,
+    }));
+  },
+  setEndTime: (setFilterState, payload) => {
     setFilterState((prev) => ({
       ...prev,
       ...payload,
@@ -39,13 +27,9 @@ const setFilterContext = {
   },
   initializeState: (setFilterState) => {
     setFilterState({
-      isCheckedSN: true,
-      isCheckedSB: true,
-      isCheckedDB: true,
-      isCheckedDN: true,
       matchDay: moment(),
-      startTime: null,
-      endTime: null,
+      startTime: '10:00',
+      endTime: '12:00',
       isSimilerRank: false,
     });
   },
