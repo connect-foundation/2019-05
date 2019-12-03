bc6bd46c1b8be31457676b8ba7b98fc009c66e8b
jong yoon <dhorlawhddbs@naver.com>
2019-12-03 21:52:07 +0900

    FIX: MatchRegistModal 컴포넌트를 세분화
    모달 컴포넌트의 크기가 너무 커서 세부 컴포넌트로 분할.

--- client/src/components/match/MatchRegistModal/index.jsx
+++ client/src/components/match/MatchRegistModal/index.jsx
@@ -42,52 +42,86 @@ const MatchRegistModal = () => {
   return (
     <div className={`match-regist-modal ${toggleModalVisible()}`}>
       <div className="modal-container">
-        <div className="match-regist-header">
-          <div className="header-title">
-            <p>매치 등록</p>
-          </div>
-          <div className="close-btn">
-            <button type="button" onClick={handleCloseBtn}>
-              X
-            </button>
-          </div>
-        </div>
-        <form className="match-regist-form">
-          <div className="district-section">
-            <p className="district-title">지역 :</p>
-            <select name="district">
-              {seoulDistrict.map((district) => {
-                return (
-                  <option key={district.engName} value={district.engName}>
-                    {district.korName}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div className="time-section">
-            <p className="time-title">시간 :</p>
-            <input type="date" />
-            <input type="time" />
-          </div>
-          <div className="address-section">
-            <p className="address-title">주소 :</p>
-            <input type="text" />
-          </div>
-          <div className="etc-section">
-            <p className="etc-title">비고 :</p>
-            <input
-              type="text"
-              maxLength="50"
-              placeholder="간단히 추가할 내용(50자)"
-            />
-          </div>
-          <button type="submit" className="submit-btn">
-            <p>등록하기</p>
-          </button>
-        </form>
+        <ModalHeader />
+        <ModalForm />
       </div>
       <div className="modal-background" />
+    </div>
+  );
+};
+
+const ModalHeader = () => {
+  return (
+    <div className="modal-header">
+      <div className="title">
+        <p>매치 등록</p>
+      </div>
+      <div className="close-btn">
+        <button type="button">X</button>
+      </div>
+    </div>
+  );
+};
+
+const ModalForm = () => {
+  return (
+    <form className="modal-form">
+      <DistrictSection />
+      <TimeSection />
+      <AddressSection />
+      <EtcSection />
+      <button type="submit" className="submit-btn">
+        <p>등록하기</p>
+      </button>
+    </form>
+  );
+};
+
+const DistrictSection = () => {
+  return (
+    <div className="district-section">
+      <p className="district-title">지역 :</p>
+      <select name="district">
+        {seoulDistrict.map((district) => {
+          return (
+            <option key={district.engName} value={district.engName}>
+              {district.korName}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+const TimeSection = () => {
+  return (
+    <div className="time-section">
+      <p className="time-title">시간 :</p>
+      <input type="date" />
+      <input type="time" />
+    </div>
+  );
+};
+
+const AddressSection = () => {
+  return (
+    <div className="address-section">
+      <p className="address-title">주소 :</p>
+      <input type="text" />
+    </div>
+  );
+};
+
+const EtcSection = () => {
+  return (
+    <div className="etc-section">
+      <p className="etc-title">비고 :</p>
+      <input
+        type="text"
+        maxLength="50"
+        placeholder="간단히 추가할 내용(50자)"
+      />
     </div>
   );
 };
