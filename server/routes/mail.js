480a46c2f62a363c107a37f4d6170e9c694a69ac
jong yoon <dhorlawhddbs@naver.com>
2019-12-06 11:26:33 +0900

    FIX: confilict 수정

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mailSender = require('../utils/nodemailer');
-<<<<<<< HEAD
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  mailSender
-    .setOptions({
-      to: ['seungnam2@gmail.com', 'bigrsnboy@naver.com'],
-      subject: 'correction',
-      html: '<h1>thanks for the invitation</h1>',
-=======
 
 router.post('/', function(req, res, next) {
   const { date, startTime, endTime, host, stadium, area } = req.body.matchInfo;
@@ -34,7 +25,6 @@ router.post('/', function(req, res, next) {
       ],
       subject: host.name,
       html: template,
->>>>>>> 23b27e0946f15d5e1bad5b6bcab927032b3ae301
     })
     .fireMail();
   res.send('done!');
