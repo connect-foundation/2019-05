27c9006d3ae0df436c6eab9d79b58ab2cc68c725
jong yoon <dhorlawhddbs@naver.com>
2019-12-08 20:15:25 +0900

    FIX: 지역구 이름 마커 위치 수정
    기존 이름 마커의 위치는 지역구 가운데 위치.
    
    하지만 이는 지역구의 가장 긴 너비와, 높이를 기준으로 만들어진
    사각형의 가운데이므로 실제 지역구 모양의 가운데와다르고,
    가운데라더라도 이름 마커가 지역구 안으로 들어가지 않는이슈가 존재
    
    따라서 이름 마커를 지역구별 맞는 위치로 수정.

--- client/src/components/match/MatchMap/index.jsx
+++ client/src/components/match/MatchMap/index.jsx
@@ -17,10 +17,30 @@ const markerElement = {
     '<div style="display:inline-block; text-align:center; font-size:20px; margin-top: 50px; width: 265px; heigth: 25px;"> <span>서울 지역구 풋살 매칭 현황 지도</span> </div>',
   districtName: (name) => {
     return `
-        <div style=" position:relative; display:block; width: 60px; height: 24px; left: -30px; top: -12px; text-align:center; font-size:15px;">
+        <div style=" position:relative; display:block; width: 60px; height: 24px; left: -30px; top: -12px; text-align:center; font-size:15px; ">
           <span>${name}</span>
         </div>`;
   },
+};
+
+const districtNamePostion = {
+  강서구: { x: 126.8211464, y: 37.5624572 },
+  양천구: { x: 126.8554787, y: 37.5183333 },
+  동작구: { x: 126.9509224, y: 37.5023592 },
+  서초구: { x: 127.0106605, y: 37.4794768 },
+  강남구: { x: 127.0552925, y: 37.4952773 },
+  종로구: { x: 126.9831947, y: 37.5829365 },
+  성북구: { x: 127.0209602, y: 37.5998031 },
+  강북구: { x: 127.0113472, y: 37.6357 },
+  도봉구: { x: 127.0340065, y: 37.6628831 },
+  노원구: { x: 127.0752052, y: 37.6509238 },
+  동대문구: { x: 127.0525459, y: 37.5807599 },
+  서대문구: { x: 126.9337562, y: 37.5742296 },
+  마포구: { x: 126.9069771, y: 37.5568129 },
+  구로구: { x: 126.8424324, y: 37.4925533 },
+  광진구: { x: 127.083445, y: 37.5448365 },
+  은평구: { x: 126.9275764, y: 37.616666 },
+  중구: { x: 126.9955543, y: 37.5595345 },
 };
 
 const MatchMap = () => {
@@ -106,13 +126,24 @@ const NaverMap = (props) => {
     }
   };
 
+  const getDistrictCenter = (district, name) => {
+    if (!districtNamePostion[name]) {
+      return district.getBounds().getCenter();
+    }
+    return new mapData.LatLng(
+      districtNamePostion[name].y,
+      districtNamePostion[name].x
+    );
+  };
+
   let districtMarker;
   const createDistrictNameMarker = (district) => {
+    const name = district.property_sig_kor_nm;
     districtMarker = new mapData.Marker({
-      position: district.getBounds().getCenter(),
+      position: getDistrictCenter(district, name),
       clickable: true,
       icon: {
-        content: markerElement.districtName(district.property_sig_kor_nm),
+        content: markerElement.districtName(name),
       },
       map: naverMap,
     });
