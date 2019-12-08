86c868b2f6c684daa2f8251bc9dae435cc42278d
jong yoon <dhorlawhddbs@naver.com>
2019-12-09 04:07:02 +0900

    FEATURE: 지역구 선택시 마커 고정
    선택한 지역구의 마커는 사라지지 않도록 고정
    
    이외에 리펙토링 진행

--- client/src/components/match/MatchMap/index.jsx
+++ client/src/components/match/MatchMap/index.jsx
@@ -6,69 +6,99 @@ import './index.scss';
 
 let districtInfo = {
   종로구: {
-    namePosition: { x: 126.9831947, y: 37.5829365 },
+    namePosition: { x: 126.9672221, y: 37.5883778 },
     isSelected: false,
   },
-  중구: { namePosition: { x: 126.9955543, y: 37.5595345 }, isSelected: false },
-  용산구: { isSelected: false },
-  성동구: { isSelected: false },
-  광진구: { namePosition: { x: 127.083445, y: 37.5448365 }, isSelected: false },
+  중구: { namePosition: { x: 126.9805817, y: 37.5666103 }, isSelected: false },
+  용산구: {
+    namePosition: { x: 126.9605354, y: 37.5383031 },
+    isSelected: false,
+  },
+  성동구: {
+    namePosition: { x: 127.0216469, y: 37.5562685 },
+    isSelected: false,
+  },
+  광진구: {
+    namePosition: { x: 127.0690254, y: 37.5508249 },
+    isSelected: false,
+  },
   동대문구: {
-    namePosition: { x: 127.0525459, y: 37.5807599 },
+    namePosition: { x: 127.0320798, y: 37.5889219 },
     isSelected: false,
   },
-  중랑구: { isSelected: false },
+  중랑구: {
+    namePosition: { x: 127.0758919, y: 37.5992591 },
+    isSelected: false,
+  },
   성북구: {
-    namePosition: { x: 127.0209602, y: 37.5998031 },
+    namePosition: { x: 126.9983009, y: 37.608507 },
     isSelected: false,
   },
-  강북구: { namePosition: { x: 127.0113472, y: 37.6357 }, isSelected: false },
+  강북구: {
+    namePosition: { x: 126.9948677, y: 37.6427686 },
+    isSelected: false,
+  },
   도봉구: {
-    namePosition: { x: 127.0340065, y: 37.6628831 },
+    namePosition: { x: 127.0161403, y: 37.6683185 },
     isSelected: false,
   },
   노원구: {
-    namePosition: { x: 127.0752052, y: 37.6509238 },
+    namePosition: { x: 127.0587257, y: 37.6509238 },
     isSelected: false,
   },
-  은평구: { namePosition: { x: 126.9275764, y: 37.616666 }, isSelected: false },
+  은평구: {
+    namePosition: { x: 126.909037, y: 37.6231925 },
+    isSelected: false,
+  },
   서대문구: {
-    namePosition: { x: 126.9337562, y: 37.5742296 },
+    namePosition: { x: 126.9122767, y: 37.5807599 },
     isSelected: false,
   },
   마포구: {
-    namePosition: { x: 126.9069771, y: 37.5568129 },
+    namePosition: { x: 126.8843178, y: 37.5638889 },
     isSelected: false,
   },
   양천구: {
-    namePosition: { x: 126.8554787, y: 37.5183333 },
+    namePosition: { x: 126.8348793, y: 37.5281454 },
     isSelected: false,
   },
   강서구: {
-    namePosition: { x: 126.8211464, y: 37.5624572 },
+    namePosition: { x: 126.8061737, y: 37.5687873 },
     isSelected: false,
   },
   구로구: {
-    namePosition: { x: 126.8424324, y: 37.4925533 },
+    namePosition: { x: 126.8259529, y: 37.4990907 },
     isSelected: false,
   },
-  금천구: { isSelected: false },
-  영등포구: { isSelected: false },
+  금천구: { namePosition: { x: 126.8816311, y: 37.466943 }, isSelected: false },
+  영등포구: {
+    namePosition: { x: 126.8911842, y: 37.5290465 },
+    isSelected: false,
+  },
   동작구: {
-    namePosition: { x: 126.9509224, y: 37.5023592 },
+    namePosition: { x: 126.9296364, y: 37.5099851 },
     isSelected: false,
   },
-  관악구: { isSelected: false },
+  관악구: {
+    namePosition: { x: 126.9268898, y: 37.4723928 },
+    isSelected: false,
+  },
   서초구: {
-    namePosition: { x: 127.0106605, y: 37.4794768 },
+    namePosition: { x: 126.9886879, y: 37.4887396 },
     isSelected: false,
   },
   강남구: {
-    namePosition: { x: 127.0552925, y: 37.4952773 },
+    namePosition: { x: 127.0456795, y: 37.4969117 },
     isSelected: false,
   },
-  송파구: { isSelected: false },
-  강동구: { isSelected: false },
+  송파구: {
+    namePosition: { x: 127.0985512, y: 37.5088957 },
+    isSelected: false,
+  },
+  강동구: {
+    namePosition: { x: 127.1280769, y: 37.5589902 },
+    isSelected: false,
+  },
 };
 
 const NAVER_MAP_API_REQUEST_URL = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_NCP_CLIENT_ID}`;
@@ -78,8 +108,9 @@ const markerElement = {
   mapTitle:
     '<div style="display:inline-block; text-align:center; font-size:20px; margin-top: 50px; width: 265px; heigth: 25px;"> <span>서울 지역구 풋살 매칭 현황 지도</span> </div>',
   districtName: (name) => {
+    const width = (name.length + 2) * 10;
     return `
-        <div style=" position:relative; display:block; width: 60px; height: 24px; left: -30px; top: -12px; text-align:center; font-size:15px; ">
+        <div style=" position:relative; display:block; width:${width}px; height:24px; text-align:center; font-size:15px;">
           <span>${name}</span>
         </div>`;
   },
@@ -121,6 +152,17 @@ const NaverMap = (props) => {
   /* eslint react/prop-types: 0 */
   const { mapData, districtData } = props;
   const [naverMap, setNaverMap] = useState(undefined);
+
+  const createMapTitleMarker = () => {
+    return new mapData.Marker({
+      position: mapData.Position.TOP_LEFT,
+      clickable: false,
+      icon: {
+        content: markerElement.mapTitle,
+      },
+      map: naverMap,
+    });
+  };
 
   const mapInitOptions = {
     zoom: 11,
@@ -181,92 +223,119 @@ const NaverMap = (props) => {
     }
   };
 
-  const getDistrictCenter = (district, name) => {
-    if (districtInfo[name].namePosition === undefined) {
-      return district.getBounds().getCenter();
-    }
-    return new mapData.LatLng(
-      districtInfo[name].namePosition.y,
-      districtInfo[name].namePosition.x
-    );
+  // 현재 마우스 이벤트에 따라 나오는 마커
+  let districtMarker;
+  // 선택한 지역구의 마커들
+  let selectedDistrictMarker = {};
+
+  const showSelectedDistrictMarker = () => {
+    Object.values(selectedDistrictMarker).forEach((marker) => {
+      marker.setMap(naverMap);
+    });
   };
 
-  let districtMarker;
+  const hideAllDistrictMarker = () => {
+    Object.values(selectedDistrictMarker).forEach((marker) => {
+      marker.setMap(null);
+    });
+  };
+
+  // 마커와 지역구에 등록할 공통 이벤트
+  const mouseoverEvent = (target) => {
+    const curDistrictName = target.property_sig_kor_nm;
+    if (districtInfo[curDistrictName].isSelected) {
+      return;
+    }
+    target.setStyle(setDistrictOption('mouseover'));
+  };
+
+  const mouseoutEvent = (target) => {
+    const curDistrictName = target.property_sig_kor_nm;
+    showSelectedDistrictMarker();
+    if (districtInfo[curDistrictName].isSelected) {
+      return;
+    }
+    target.setStyle(setDistrictOption());
+  };
+
+  const clickEvent = (target) => {
+    const curDistrictName = target.property_sig_kor_nm;
+    hideAllDistrictMarker();
+    const preDistrictInfo = { ...districtInfo };
+    const isSelected = !preDistrictInfo[curDistrictName].isSelected;
+    preDistrictInfo[curDistrictName].isSelected = isSelected;
+    districtInfo = preDistrictInfo;
+    const curAction = isSelected ? 'click' : '';
+    target.setStyle(setDistrictOption(curAction));
+    if (isSelected) {
+      if (!selectedDistrictMarker[curDistrictName]) {
+        selectedDistrictMarker[curDistrictName] = districtMarker;
+      }
+      showSelectedDistrictMarker();
+      return;
+    }
+    if (selectedDistrictMarker[curDistrictName]) {
+      const newMarkers = {};
+      Object.entries(selectedDistrictMarker).forEach(([markerName, marker]) => {
+        if (markerName !== curDistrictName) {
+          newMarkers[markerName] = marker;
+        }
+      });
+      selectedDistrictMarker = { ...newMarkers };
+    }
+    showSelectedDistrictMarker();
+  };
+
+  // 지역구 이름 마커 생성 함수
   const createDistrictNameMarker = (district) => {
-    const name = district.property_sig_kor_nm;
+    const curDistrictName = district.property_sig_kor_nm;
     districtMarker = new mapData.Marker({
-      position: getDistrictCenter(district, name),
+      position: new mapData.LatLng(
+        districtInfo[curDistrictName].namePosition.y,
+        districtInfo[curDistrictName].namePosition.x
+      ),
       clickable: true,
       icon: {
-        content: markerElement.districtName(name),
+        content: markerElement.districtName(curDistrictName),
       },
       map: naverMap,
     });
-    addEvent(true, districtMarker, 'mouseover', () => {
-      const curDistrict = district.property_sig_kor_nm;
-      if (districtInfo[curDistrict].isSelected) {
-        return;
-      }
-      district.setStyle(setDistrictOption('mouseover'));
-    });
-    addEvent(true, districtMarker, 'mouseout', () => {
-      const curDistrict = district.property_sig_kor_nm;
-      if (districtInfo[curDistrict].isSelected) {
-        return;
-      }
-      district.setStyle(setDistrictOption());
-    });
-    addEvent(true, districtMarker, 'click', () => {
-      const selectedDistrict = district.property_sig_kor_nm;
-      const preDistrictInfo = { ...districtInfo };
-      preDistrictInfo[selectedDistrict].isSelected = !preDistrictInfo[
-        selectedDistrict
-      ].isSelected;
-      districtInfo = preDistrictInfo;
-      const curAction = preDistrictInfo[selectedDistrict].isSelected
-        ? 'click'
-        : '';
-      district.setStyle(setDistrictOption(curAction));
-      console.log(districtInfo);
-    });
+    addEvent(
+      true,
+      districtMarker,
+      'mouseover',
+      mouseoverEvent.bind(null, district)
+    );
+    addEvent(
+      true,
+      districtMarker,
+      'mouseout',
+      mouseoutEvent.bind(null, district)
+    );
+    addEvent(true, districtMarker, 'click', clickEvent.bind(null, district));
   };
 
+  // 지역구에 등록할 이벤트
   const handleDistrictMouseoverEvent = (e) => {
     if (districtMarker) {
       districtMarker.setMap(null);
     }
-    const curDistrict = e.feature.property_sig_kor_nm;
-    if (districtInfo[curDistrict].isSelected) {
-      return;
-    }
-    const selecteDistrictOption = setDistrictOption('mouseover');
-    e.feature.setStyle(selecteDistrictOption);
-    createDistrictNameMarker(e.feature, selecteDistrictOption);
+    mouseoverEvent(e.feature);
+    createDistrictNameMarker(e.feature);
   };
 
   const handleDistrictMouseoutEvent = (e) => {
-    const curDistrict = e.feature.property_sig_kor_nm;
-    if (districtInfo[curDistrict].isSelected) {
-      return;
-    }
-    e.feature.setStyle(setDistrictOption());
+    mouseoutEvent(e.feature);
+  };
+
+  const handleDistrictClickEvent = (e) => {
+    clickEvent(e.feature);
   };
 
   const handleDistrictMousemoveEvent = () => {
     if (districtMarker) {
       districtMarker.setMap(null);
     }
-  };
-
-  const createMapTitleMarker = () => {
-    return new mapData.Marker({
-      position: mapData.Position.TOP_LEFT,
-      clickable: false,
-      icon: {
-        content: markerElement.mapTitle,
-      },
-      map: naverMap,
-    });
   };
 
   useEffect(() => {
@@ -280,19 +349,7 @@ const NaverMap = (props) => {
       naverMap.data.setStyle(setDistrictOption());
       addEvent(false, naverMap.data, 'mouseover', handleDistrictMouseoverEvent);
       addEvent(false, naverMap.data, 'mouseout', handleDistrictMouseoutEvent);
-      addEvent(false, naverMap.data, 'click', (e) => {
-        const selectedDistrict = e.feature.property_sig_kor_nm;
-        const preDistrictInfo = { ...districtInfo };
-        preDistrictInfo[selectedDistrict].isSelected = !preDistrictInfo[
-          selectedDistrict
-        ].isSelected;
-        districtInfo = { ...preDistrictInfo };
-        const curAction = preDistrictInfo[selectedDistrict].isSelected
-          ? 'click'
-          : '';
-        e.feature.setStyle(setDistrictOption(curAction));
-        console.log(districtInfo);
-      });
+      addEvent(false, naverMap.data, 'click', handleDistrictClickEvent);
       addEvent(true, naverMap, 'mousemove', handleDistrictMousemoveEvent);
     }
     /* eslint react-hooks/exhaustive-deps: 0 */
