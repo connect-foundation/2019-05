ef59818e9b2496419e7d16512868edaa78311273
Sam Rho <seungnam2@gmail.com>
2019-12-10 18:43:12 +0900

    FEATURE: email 전송 테스트를위한 '/' get endpoint 엶.
    1. 간단한 이메일 템플릿을 작성하였다.
    2. 테스트를 위해 url 창에서 직접 접속할 수 있도록 routes/mail.js
       를 수정하였다.

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const mailSender = require('../utils/nodemailer');
+const template = require('../utils/mailTemplate');
+router.get('/', (req, res, next) => {
+  mailSender
+    .setOptions({
+      to: ['seungnam2@gmail.com'],
+      subject: 'seungnam',
+      html: template,
+    })
+    .fireMail();
+  res.send('done!');
+});
 
 router.post('/', function(req, res, next) {
   const { date, startTime, endTime, host, stadium, area } = req.body.matchInfo;
@@ -9,20 +20,14 @@ router.post('/', function(req, res, next) {
       <li>date:${date}</li>
       <li>startTime:${startTime}</li>
       <li>endTime:${endTime}</li>
-      <li>host name:${host.name}</li>
+      <li>host name:${host.name}</li> 
       <li>stadium:${stadium}</li>
       <li>area:${area}</li>
     </ul>
   `;
   mailSender
     .setOptions({
-      to: [
-        // 'we.underdoggs@gmail.com',
-        // 'eastgerm_8@naver.com',
-        // 'dhorlawhddbs@naver.com',
-        // 'bigrsnboy@naver.com',
-        'seungnam2@gmail.com',
-      ],
+      to: ['seungnam2@gmail.com'],
       subject: host.name,
       html: template,
     })
