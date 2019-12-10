72d69b7756a150ed7bbd26fb2bd1aa47dab1a764
eastgerm <dowk0331@gmail.com>
2019-12-10 10:20:09 +0900

    FEATURE: passport 네이버 로그인 시 우리 회원DB를 순회하여 가입유무를 판단
    prisma-client에 있는 model을 통하여 확인

--- server/middlewares/passport.js
+++ server/middlewares/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const NaverStrategy = require('passport-naver').Strategy;
 const { env } = process;
+//
+const { prisma } = require('../generated/prisma-client');
 
 const prodClientInfo = {
   clientID: env.NAVER_CLIENT_ID,
@@ -23,17 +25,11 @@ passport.deserializeUser((user, done) => done(null, user));
 passport.use(
   new NaverStrategy(
     clientInfo,
-    (accessToken, refreshToken, profile, done) => {
-      /*
-      QuickKick 회원 DB에 저장되어 있는지 검사하는 부분 필요
-      아래는 임시로 1개의 아이디와의 비교만 하고 있음
-       */
+    async (accessToken, refreshToken, profile, done) => {
+      const playerId = profile.id;
+      const players = await prisma.players({where: {playerId}});
+      if(!players.length) await prisma.createPlayer({playerId});
       return done(null, profile);
-      // if (profile.id === '113927503') return done(null, profile);
-      // else {
-      //   console.log('없는 회원입니다');
-      //   return done(null, null);
-      // }
     }
   )
 );
