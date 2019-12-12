0559434ccc5bbee7f2c564de341c1f6d1abaf910
eastgerm <dowk0331@gmail.com>
2019-12-12 14:02:50 +0900

    FEATURE: User Context 파일 분리 및 구조 변경
    Actions, InitialState, Reducer, Context, Provider 모두 각각의 파일을 가지며
    index.js를 통해 User 접두어를 붙인 채 export 된다

--- client/src/contexts/User/Reducer.js
+++ client/src/contexts/User/Reducer.js
@@ -1,18 +1,15 @@
-const playerActions = {
-  LOGIN: 'LOGIN',
-  LOGOUT: 'LOGOUT',
-};
+import actions from './Actions';
 
-const playerReducer = (state, action) => {
+const Reducer = (state, action) => {
   /* eslint indent: ["error", 2, { "SwitchCase": 1 }] */
   switch (action.type) {
-    case playerActions.LOGIN:
+    case actions.LOGIN:
       return { ...state, playerId: action.payload };
-    case playerActions.LOGOUT:
+    case actions.LOGOUT:
       return { ...state, playerId: null };
     default:
       throw new Error('Unhandled action!');
   }
 };
 
-export { playerActions, playerReducer };
+export default Reducer;
