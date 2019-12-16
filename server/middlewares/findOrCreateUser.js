a04ba1f6b9c3308cd537194368ce568fef59d503
eastgerm <dowk0331@gmail.com>
2019-12-16 17:32:37 +0900

    FEATURE: 로그인 후 가입여부 확인 시 playId 이외로 authProvider까지 추가해서 확인

--- server/middlewares/findOrCreateUser.js
+++ server/middlewares/findOrCreateUser.js
@@ -2,9 +2,11 @@ const { prisma } = require('../generated/prisma-client');
 const convertToString = require('../utils/convertToString');
 
 const findOrCreateUser = async (accessToken, refreshToken, profile, done) => {
+  const authProvider = profile.provider.toUpperCase();
+  console.log(profile);
   const playerId = convertToString(profile.id);
-  const players = await prisma.players({where: {playerId}});
-  if(!players.length) await prisma.createPlayer({playerId});
+  const players = await prisma.players({where: {authProvider, playerId}});
+  if(!players.length) await prisma.createPlayer({authProvider, playerId});
   return done(null, profile);
 };
 
