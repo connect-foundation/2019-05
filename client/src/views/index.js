2852f16b4ae4d2b1f8be66afb5d31503f40ad7f4
eastgerm <dowk0331@gmail.com>
2019-12-19 02:08:49 +0900

    FIX: 사용되지 않는 view 삭제

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,7 +1,6 @@
 import home from './home';
 import match from './match';
 import myteam from './myteam';
-import mypage from './mypage';
 import error from './error';
 
-export { home, match, myteam, mypage, error };
+export { home, match, myteam, error };
