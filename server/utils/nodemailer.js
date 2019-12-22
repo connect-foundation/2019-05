4ff0e4dbfe3f0c53c8e7f8a977b176e1968dfef7
Sam Rho <seungnam2@gmail.com>
2019-12-22 23:01:04 +0900

    FIX: 유저가 폰번호를 하이픈을 넣어 입력했을 때 그것을 없애주게 함.
    replace(/-/g, '')을 이용하여 작업을 수행하였다.

--- server/utils/nodemailer.js
+++ server/utils/nodemailer.js
@@ -17,20 +17,13 @@ const TRANSPORT_CONFIG = {
   maxConnections: 10,
   maxMessages: 10,
 };
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: env.EMAIL_USER,
-    pass: env.EMAIL_PW,
-  },
-});
+
 const mailSender = {
   mailOptions: { from: env.UNDERDOGGS_EMAIL_ADDR },
-  // transporter,
   transporter: nodemailer.createTransport(smtpPool(TRANSPORT_CONFIG)),
   fireMail({ to, subject, html }) {
     this.mailOptions = { ...this.mailOptions, to, subject, html };
-    this.transporter.sendMail(this.mailOptions, (err, res) => {
+    this.transporter.sendMail(this.mailOptions, (err, _) => {
       if (err) console.error();
       console.log(`email has been sent!`);
     });
